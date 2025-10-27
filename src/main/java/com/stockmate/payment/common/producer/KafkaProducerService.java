5a50d404c3a64d467da18120f88403702b96e73a
et007693 <thdckdyd123@naver.com>
2025-10-27 19:05:25 +0900

    [DOCS] 결제 결과 order에 전송

--- src/main/java/com/stockmate/payment/common/producer/KafkaProducerService.java
+++ src/main/java/com/stockmate/payment/common/producer/KafkaProducerService.java
@@ -1,11 +1,14 @@
 package com.stockmate.payment.common.producer;
 
-import com.stockmate.payment.api.payment.dto.DepositDeductionSuccessEvent;
+import com.stockmate.payment.api.payment.dto.PayResponseEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
 
 @Service
 @RequiredArgsConstructor
@@ -14,14 +17,51 @@ public class KafkaProducerService {
 
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
-    @Value("${kafka.topics.deposit-deduction-success}")
-    private String depositDeductionSuccessTopic;
+    @Value("${kafka.topics.pay-success}")
+    private String paySuccessTopic;
 
-    @Value("${kafka.topics.deposit-deduction-failed}")
-    private String depositDeductionFailedTopic;
+    @Value("${kafka.topics.pay-failed}")
+    private String payFailedTopic;
 
-    public void sendDepositDeductionSuccess(DepositDeductionSuccessEvent event) {
-        
+    public void sendPaySuccess(PayResponseEvent event) {
+        CompletableFuture<SendResult<String, Object>> future = kafkaTemplate.send(
+                paySuccessTopic,
+                event.getOrderId().toString(),
+                event
+        );
+
+        future.whenComplete((result, ex) -> {
+            if (ex == null) {
+                log.info("결제 성공 이벤트 발송 성공 - 토픽: {}, 파티션: {}, 오프셋: {}, Order ID: {}",
+                        result.getRecordMetadata().topic(),
+                        result.getRecordMetadata().partition(),
+                        result.getRecordMetadata().offset(),
+                        event.getOrderId());
+            } else {
+                log.error("결제 성공 요청 이벤트 발송 실패 - Order ID: {}, 에러: {}",
+                        event.getOrderId(), ex.getMessage(), ex);
+            }
+        });
     }
 
+    public void sendPayFailed(PayResponseEvent event) {
+        CompletableFuture<SendResult<String, Object>> future = kafkaTemplate.send(
+                paySuccessTopic,
+                event.getOrderId().toString(),
+                event
+        );
+
+        future.whenComplete((result, ex) -> {
+            if (ex == null) {
+                log.info("결제 실패 이벤트 발송 성공 - 토픽: {}, 파티션: {}, 오프셋: {}, Order ID: {}",
+                        result.getRecordMetadata().topic(),
+                        result.getRecordMetadata().partition(),
+                        result.getRecordMetadata().offset(),
+                        event.getOrderId());
+            } else {
+                log.error("결제 실패 요청 이벤트 발송 실패 - Order ID: {}, 에러: {}",
+                        event.getOrderId(), ex.getMessage(), ex);
+            }
+        });
+    }
 }
