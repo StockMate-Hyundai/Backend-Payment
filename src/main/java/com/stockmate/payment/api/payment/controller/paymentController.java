164e4f38f2ae8a9d104e9dfe94e75c0185669da3
송창용 <thdckdyd123@naver.com>
2025-10-29 09:54:09 +0900

    [REFAC] 결제 요청 구조 변경

--- src/main/java/com/stockmate/payment/api/payment/controller/paymentController.java
+++ src/main/java/com/stockmate/payment/api/payment/controller/paymentController.java
@@ -1,10 +1,13 @@
 package com.stockmate.payment.api.payment.controller;
 
+import com.stockmate.payment.api.payment.dto.PayRequestEvent;
+import com.stockmate.payment.api.payment.dto.PayResponseEvent;
 import com.stockmate.payment.api.payment.entity.Balance;
 import com.stockmate.payment.api.payment.service.PaymentService;
 import com.stockmate.payment.common.config.security.SecurityUser;
 import com.stockmate.payment.common.response.ApiResponse;
 import com.stockmate.payment.common.response.SuccessStatus;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,6 +23,7 @@ import org.springframework.web.bind.annotation.*;
 public class paymentController {
     private final PaymentService paymentService;
 
+    @Operation(summary = "예치금 조회 API", description = "현재 계정의 예치금을 조회합니다.")
     @GetMapping("/amount")
     public ResponseEntity<ApiResponse<Balance>> getDepositAmount(
             @AuthenticationPrincipal SecurityUser securityUser
@@ -29,6 +33,7 @@ public class paymentController {
         return ApiResponse.success(SuccessStatus.DEPOSIT_CHECK_SUCCESS, response);
     }
 
+    @Operation(summary = "예치금 충전 API", description = "현재 계정에 예치금을 입급합니다.")
     @PostMapping("/charge")
     public ResponseEntity<ApiResponse<Void>> depositCharge(
             @RequestParam Long amount,
@@ -37,5 +42,15 @@ public class paymentController {
         Long userId = securityUser.getMemberId();
         paymentService.depositCharge(userId, amount);
         return ApiResponse.success_only(SuccessStatus.DEPOSIT_CHARGE_SUCCESS);
+    }
+
+    @Operation(summary = "예치금 결제 요청 API", description = "예치금으로 결제 요청합니다.")
+    @PostMapping("/pay")
+    public ResponseEntity<ApiResponse<PayResponseEvent>> depositPay(
+            @RequestBody PayRequestEvent payRequestEvent,
+            @AuthenticationPrincipal SecurityUser securityUser
+            ) {
+        PayResponseEvent response = paymentService.handleDepositPayRequest(payRequestEvent);
+        return ApiResponse.success(SuccessStatus.DEPOSIT_PAY_SUCCESS, response);
     }
 }
