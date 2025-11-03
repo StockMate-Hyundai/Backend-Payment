d2c41f12df1115fb1ed46116e0ba98edba4bd62c
송창용 <thdckdyd123@naver.com>
2025-11-03 15:00:11 +0900

    [FIX] Lazy Loading 해결

--- src/main/java/com/stockmate/payment/api/payment/repository/DepositTransactionRepository.java
+++ src/main/java/com/stockmate/payment/api/payment/repository/DepositTransactionRepository.java
@@ -3,9 +3,11 @@ package com.stockmate.payment.api.payment.repository;
 import com.stockmate.payment.api.payment.entity.DepositTransaction;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DepositTransactionRepository extends JpaRepository<DepositTransaction, Long> {
+    @EntityGraph(attributePaths = {"payment"})
     Page<DepositTransaction> findAllByUserId(
             Long userId,
             Pageable pageable
